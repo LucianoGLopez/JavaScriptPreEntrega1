dc17ca621bb77faa17d2009b5bad8f22784954f3
Luciano Lopez <lopez.luciano@outlook.com>
2023-01-20 17:37:43 -0300

    segunda pre entrega

--- js/main.js
+++ js/main.js
@@ -1,59 +1,89 @@
-function solicitarUsuarioYEdad() {
-    let usuario = prompt("Ingrese su nombre:");
-    let edad = parseInt(prompt("Ingrese su edad:"));
-
-    if (edad >= 18) {
-        alert("Bienvenido/a " + usuario);
-    } else {
-        while (edad < 18) {
-            alert("Acceso denegado. Usted es menor de edad")
-            usuario = prompt("Ingrese su nombre:");
-            edad = parseInt(prompt("Ingrese su edad:"));
-        }
-        while ((isNaN(edad)) || (edad < 18)) {
-            alert("Acceso denegado.");
-            usuario = prompt("Ingrese su nombre:");
-            edad = parseInt(prompt("Ingrese correcatamente edad:"));
-        }
+// SIMULADOR PLAZO FIJO
+class Cliente {
+    constructor(nombre, dni) {
+        this.nombre = nombre;
+        this.dni = dni;
     }
 }
 
-function calcularPrecioFinalEnCuotas() {
-    while (resultadoPrecioFinal !== "NO" && resultadoPrecioFinal !== "No" && resultadoPrecioFinal !== "no") {
-
-        const montoDelProducto = parseFloat(prompt("Por favor ingrese el monto del producto seleccionado:"));
-        let cuotas = prompt("Ingrese el número de cuotas (3, 6, 12 cuotas)");
-
-        while (cuotas !== "3" && cuotas !== "6" && cuotas !== "12") {
-            cuotas = prompt("Ingrese correctamente el número de cuotas (3, 6, 12 cuotas)");
+//Funciones
+const obtenerIndiceDeClientePorDni = (dni) => {
+    let indiceCliente = 0;
+    for (let i = 0; i < clientes.length; i++) {
+        if (clientes[i].dni === dni) {
+            indiceCliente = i;
+            break;
         }
-
-        switch (cuotas) {
-            case "3":
-                subtotal = montoDelProducto * 15 / 100;
-                resultadoPrecioFinal = subtotal + montoDelProducto;
-                break
-
-            case "6":
-                subtotal = montoDelProducto * 30 / 100;
-                resultadoPrecioFinal = subtotal + montoDelProducto;
-                break
-
-            case "12":
-                subtotal = montoDelProducto * 45 / 100;
-                resultadoPrecioFinal = subtotal + montoDelProducto;
-                break
-        }
-
-        alert("El precio final del producto en " + cuotas + " cuotas es de: $" + resultadoPrecioFinal);
-
-        resultadoPrecioFinal = prompt("Desea agregar otro producto (SI / NO)");
     }
+    return indiceCliente;
 }
 
-let resultadoPrecioFinal = "";
+const clienteExiste = (dni) => {
+    let encontrado = false;
+    for (const cliente of clientes) {
+        if (cliente.dni === dni) {
+            encontrado = true;
+            break;
+        }
+    }
+    return encontrado;
+}
 
-solicitarUsuarioYEdad();
-calcularPrecioFinalEnCuotas()
+const solicitarDniUsuario = () => {
+    let dni = prompt("Por favor ingrese DNI del cliente");
 
+    while (!clienteExiste(dni)) {
+        dni = prompt("Acceso denegado. DNI incorrecto. Ingrese correctamente el DNI");
+    }
+    
+    const indiceCliente = obtenerIndiceDeClientePorDni(dni);
+    alert("Bienvenido/a " + clientes[indiceCliente].nombre);
+}
 
+const calcularPlazoFijo = (porcentaje, anual, mesesDelPlazoFijo) => {
+    return (monto) => ((monto * porcentaje) / 100) / anual * mesesDelPlazoFijo;
+}
+
+// // INICIA PROGRAMA
+alert("SIMULADOR DE PLAZO FIJO")
+
+const clientes = [
+    new Cliente("Luciano", "111222333"),
+    new Cliente("Fiamma", "222333444"),
+    new Cliente("Anakin", "333444555"),
+];
+
+solicitarDniUsuario();
+
+const plazoFijoUnMes = calcularPlazoFijo(70, 12, 1);
+const plazoFijoTresMeses = calcularPlazoFijo(76, 12, 3);
+const plazoFijoDoceMeses = calcularPlazoFijo(85, 12, 12);
+
+let monto = parseInt(prompt("Ingrese el monto que desea invertir en Plazo Fijo:"));
+let simulador = prompt("Elija el plazo estimado: 1- UN MES (70% anual) | 2- TRES MESES (76% anual) | 3- DOCE MESES (85% anual) | 4- Salir");
+
+while (simulador !== "Salir" && simulador !== "salir" && simulador !== "SALIR" && simulador !== "4") {
+    switch (simulador) {
+        case "1":
+            plazoFijoUnMes();
+            alert("Por su inversión de $" + monto + " usted recibirá en un mes la suma de $" + (plazoFijoUnMes(monto) + monto));
+            break;
+
+        case "2":
+            plazoFijoTresMeses();
+            alert("Por su inversión de $" + monto + " usted recibirá en tres meses la suma de $" + (plazoFijoTresMeses(monto) + monto));
+            break;
+
+        case "3":
+            plazoFijoDoceMeses();
+            alert("Por su inversión de $" + monto + " usted recibirá en doce meses la suma de $" + (plazoFijoDoceMeses(monto) + monto));
+            break;
+
+        default:
+            alert("Opción incorrecta");
+            break;
+    }
+
+    simulador = prompt("Elija el plazo estimado: 1- UN MES (70% anual) | 2- TRES MESES (76% anual) | 3- DOCE MESES (85% anual) | 4- Salir");
+}
+alert("Gracias por utilizar nuestros servicios");
